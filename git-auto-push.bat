0db32208b5d339e6c3bb83b00929580ef2bb8881
CALLISTO LOM <callisto.lom@gmail.com>
2025-04-11 07:09:48 +0000

    TEST5 Daily automatic update Fri 04/11/2025

--- git-auto-push.bat
+++ git-auto-push.bat
@@ -1,7 +1,8 @@
 @echo off
-echo Starting git operations at %date% %time% >> C:\xrt\git-log.txt
+echo !!! Starting git operations at %date% %time% >> C:\xrt\git-log.txt
 cd /d C:\xrt
 "C:\Program Files\Git\cmd\git.exe" add . >> C:\xrt\git-log.txt 2>&1
-"C:\Program Files\Git\cmd\git.exe" commit -m "TEST3 Daily automatic update %date%" >> C:\xrt\git-log.txt 2>&1
-"C:\Program Files\Git\cmd\git.exe" push origin master >> C:\xrt\git-log.txt 2>&1
-echo Finished at %date% %time% >> C:\xrt\git-log.txt
+"C:\Program Files\Git\cmd\git.exe" add .
+"C:\Program Files\Git\cmd\git.exe" commit -m "TEST5 Daily automatic update %date%"
+"C:\Program Files\Git\cmd\git.exe" push origin master
+ECHO.
