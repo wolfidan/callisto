40c69d1c62e972b93d1a9802133cf3278a77f24d
Daniel Wolfensberger <daniel.wolfensberger@meteoswiss.ch>
2025-04-16 13:59:40 +0200

    add gh pages

--- docs/main.js
+++ docs/main.js
@@ -1,32 +1,26 @@
-// Add your dates here (or generate them dynamically if hosted by a static generator)
-const imageDates = [
-  "20240401",
-  "20240402",
-  "20240403",
-  // etc.
-];
-
-let index = imageDates.length - 1;  // Start with most recent
+let imageDates = [];
+let index = 0;
 
 function updateImage() {
   const date = imageDates[index];
-  const img = document.getElementById("plot");
-  img.src = `images/callisto_meteoswiss_${date}.png`;
+  document.getElementById("plot").src = `images/callisto_meteoswiss_${date}.png`;
 }
 
 function prevImage() {
-  if (index > 0) {
-    index--;
-    updateImage();
-  }
+  if (index > 0) { index--; updateImage(); }
 }
 
 function nextImage() {
-  if (index < imageDates.length - 1) {
-    index++;
-    updateImage();
-  }
+  if (index < imageDates.length - 1) { index++; updateImage(); }
 }
 
-document.addEventListener("DOMContentLoaded", updateImage);
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("images/images.json")
+    .then(response => response.json())
+    .then(data => {
+      imageDates = data;
+      index = imageDates.length - 1; // most recent
+      updateImage();
+    });
+});
 
