52fd731f6c72d2d204ac3364185b4ca5d2dd5a49
CALLISTO LOM <callisto.lom@gmail.com>
2025-05-05 09:38:28 +0000

    Daily automatic update Mon 05/05/2025

--- git-auto-push.bat
+++ git-auto-push.bat
@@ -2,7 +2,7 @@
 ECHO. >> C:\xrt\git-log.txt
 echo Git push at %date% %time% >> C:\xrt\git-log.txt
 cd /d C:\xrt
-"C:\Program Files\Git\cmd\git.exe" add . >> C:\xrt\git-log.txt 2>&1
+"C:\Program Files\Git\cmd\git.exe" add . >> C:\callisto\git-log.txt 2>&1
 "C:\Program Files\Git\cmd\git.exe" add .
 "C:\Program Files\Git\cmd\git.exe" commit -m "Daily automatic update %date%"
 "C:\Program Files\Git\cmd\git.exe" push origin master
